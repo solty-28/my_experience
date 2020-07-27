c9b6bb7630c0704155777f985105502a9bc49c3f
solty-28 <soltyna28@gmail.com>
2020-07-27 17:43:29 +0000

    コントローラー&ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'top#index'
+
+  get '/about' => 'top#about', as: 'about'
+
+
+  scope :user do
+  	resources :genres, only: [:index, :update, :create]
+  end
+
+  resources :reviews
+  resources :comments, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_105152) do
+ActiveRecord::Schema.define(version: 2020_07_27_155837) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_105152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
