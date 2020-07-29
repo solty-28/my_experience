59c292c61303f0d5cb8720c34888e318fa269295
solty-28 <soltyna28@gmail.com>
2020-07-29 05:07:43 +0000

    投稿機能途中

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,40 @@
 class ReviewsController < ApplicationController
   def index
+  	@reviews = Review.all
   end
 
   def show
+  	@review = Review.find(params[:id])
   end
 
   def new
+  	@review = Review.new
+  	@genres = Genre.where(user_id: current_user.id)
+  end
+
+  def create
+  	@review = Review.new(review_params)
+  	@review.user_id = current_user.id
+  	if @review.save
+  	  redirect_to review_path(@review)
+  	else
+  	  render action: :new
+  	end
+  end
+
+  def update
+  	
+  end
+
+  def destroy
+  	
   end
 
   def edit
+  end
+
+  private
+  def review_params
+  	params.require(:review).permit(:title, :review_body, :picture, :rating, :genre_id, :user_id)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   has_many :comments
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+
+  attachment :picture
 end
