3e33d3c70dfc3e3f4dc784e730ed226ae72ebae4
solty-28 <soltyna28@gmail.com>
2020-07-29 01:09:46 +0000

    devise name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 # サインイン、サインアウト後のパス指定
   def after_sign_up_path_for(resource)
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
   	root_path
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 
 end
