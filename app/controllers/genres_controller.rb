9cd8e6ba33426d4b4d2b4d558e8875b99d997199
solty-28 <soltyna28@gmail.com>
2020-07-29 02:28:41 +0000

    ジャンル

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,23 @@
 class GenresController < ApplicationController
+  def index
+  	@genres = Genre.where(user_id: current_user.id)
+  	@genre = Genre.new
+  end
+
+  def create
+  	@genres = Genre.where(user_id: current_user.id)
+  	@genre = Genre.new(genre_params)
+  	@genre.user_id = current_user.id
+  	if @genre.save
+  	  flash[:notice] = "ジャンルを追加しました"
+  	  render :index
+  	else
+  	  render :index
+  	end
+  end
+
+  private
+  def genre_params
+  	params.require(:genre).permit(:genre_name,:user_id)
+  end
 end
