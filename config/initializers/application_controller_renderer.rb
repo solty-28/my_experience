23e8ab9fc7d4b61f174ea3dbc92a2ccf6359e294
solty-28 <soltyna28@gmail.com>
2020-08-04 02:33:34 +0000

    デザイン修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'aebb48a09e1113ff5ca6be68656b485d861afa76d7e157059fc5df4c12e40cd53d8c5aa20f9557110486fe6a4f38a0a230fc08c3f731dd75a1898c15f0df0e55'
